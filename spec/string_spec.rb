8acf0250be72d29cb047190027cd88b4ea17cba2
unknown <dwilliams4@CMH-MIS-2RQ64W1.cmh.netjets.com>
2015-06-24 12:56:31 -0400

    removed a failing test

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -4,8 +4,4 @@ describe 'strings' do
   it 'knows an empty string is empty' do
     expect('').to be_empty
   end
-
-  it 'knows a string is not a symbol' do
-    expect(false).to eq true
-  end
 end
